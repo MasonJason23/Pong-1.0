a6feb17be7151e8782df6bc4d63cfc0b2766aca9
Jason Casareno <jcasareno@csumb.edu>
2022-02-13 20:56:11 -0800

    Implemented particle effect when a player scores a goal. Added a sound effect when a point is scored. Added background music. Added two modifiers, one changes ball size and the other changes both player's paddle size. Resized "Playground" models (ie. walls and plane).

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -7,11 +7,17 @@ public class Goal : MonoBehaviour
 {
     public GameObject ballPrefab;
     public Transform spawnPoint;
+    public ParticleSystem particleScore;
+    public GameObject goalSound;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Ball"))
         {
+            AudioSource audioSrc = goalSound.GetComponent<AudioSource>();
+            audioSrc.Play();
+            
+            particleScore.Play();
             GameObject instance = Instantiate(ballPrefab);
             instance.transform.position = spawnPoint.transform.position;
         }
